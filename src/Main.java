aca1cfc916b3422e3927d62f9ee1b0f701d818ff
matthieu-27 <matthieu27@pm.me>
2026-01-06 11:46:38 +0100

    feat: Implémentation update de CourseDAO

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
+import DAO.CourseDAO;
 import Enums.CourseType;
 import Models.Course;
-import Models.User;
 import Services.CourseService;
 
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) {
-        User matt = new User("Matthieu");
-        System.out.println(matt.getId() + " : " + matt.getName());
-        User tristan = new User("Tristan");
-        System.out.println(tristan.getId() + " : " + tristan.getName());
-
-        Course eps = new Course("EPS");
-        Course svt = new Course("SVT", CourseType.DISTANCIEL);
-
-        System.out.println(eps.name() + ", " + svt.name() + ":" + svt.type());
+//        User matt = new User("Matthieu");
+//        System.out.println(matt.getId() + " : " + matt.getName());
+//        User tristan = new User("Tristan");
+//        System.out.println(tristan.getId() + " : " + tristan.getName());
+//
+//        Course eps = new Course("EPS");
+//        Course svt = new Course("SVT", CourseType.DISTANCIEL);
+//
+//        System.out.println(eps.name() + ", " + svt.name() + ":" + svt.type());
+        // Lecture via Service (DB)
         try {
             ArrayList<Course> courses = CourseService.getAllCourses();
             for(Course c: courses){
@@ -26,6 +28,38 @@ public class Main {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        // Lecture via l'ArrayList statique de Course
+        ArrayList<Course> staticCourses = Course.coursesList;
+        for(Course c: staticCourses){
+            System.out.println(c.toString());
+        }
+
+
+        // Utilisation DAO
+
+        // test find
+
+        try {
+            CourseDAO daoF = new CourseDAO();
+            Course testF = daoF.find(11);
+            System.out.println(testF.toString());
+        } catch (SQLException e ){
+            e.printStackTrace();
+        }
+
+
+        try{
+            CourseDAO dao = new CourseDAO();
+            Course cobble = new Course("Ruby", "Almost as cool as python", 40, CourseType.DISTANCIEL, BigDecimal.valueOf(75.342));
+            Course cobbleDb = dao.create(cobble);
+            System.out.println(cobbleDb.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
 
 
     }
