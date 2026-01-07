c2a820edce9d254d61b97c82a3d484b0a8c51ef2
matthieu-27 <matthieu27@pm.me>
2026-01-07 10:05:18 +0100

    fix: Mise à jour du modèle User

--- src/Models/User.java
+++ src/Models/User.java
@@ -1,16 +1,89 @@
 package Models;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
 
+/**
+ * Classe représentant un utilisateur authentifié
+ * @author Matthieu Couderc
+ * @version 2.0
+ */
 public class User {
-
-    private static final AtomicInteger ID_FACTORY = new AtomicInteger();
-    private final int id;
+    private int id;
     private String name;
+    private String email;
+    private String password;
 
-    public User(String name){
-        this.id = ID_FACTORY.getAndIncrement();
+    public static ArrayList<User> usersList = new ArrayList<>();
+
+    /**
+     * Constructeur complet pour un utilisateur
+     * @param id: identifiant unique
+     * @param name: nom complet de l'utilisateur
+     * @param email: email de l'utilisateur
+     * @param password: mot de passe (stocké hashé en production)
+     */
+    public User(int id, String name, String email, String password) {
+        this.id = id;
         this.name = name;
+        this.email = email;
+        this.password = password;
+        usersList.add(this);
+    }
+
+    /**
+     * Constructeur pour création d'utilisateur (sans id)
+     */
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * Méthode d'authentification
+     * @param email: email de connexion
+     * @param password: mot de passe
+     * @return User si authentification réussie, null sinon
+     */
+    public static User authenticate(String email, String password) {
+        for (User user : usersList) {
+            if (user.email.equals(email) && user.password.equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Méthode d'inscription
+     * @param name: nom complet
+     * @param email: email
+     * @param password: mot de passe
+     * @return Nouveau User créé
+     */
+    public static User register(String name, String email, String password) {
+        User newUser = new User(name, email, password);
+        usersList.add(newUser);
+        return newUser;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    /* GETTERS AND SETTERS */
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -21,7 +94,19 @@ public class User {
         this.name = name;
     }
 
-    public final int getId() {
-        return id;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
